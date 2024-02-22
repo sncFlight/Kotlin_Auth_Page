c990f00ecec92fff40b23ea6d5e345295f0d5474
d.shaikhullin <d.shaikhullin@mail.business.ru>
2024-02-22 17:49:07 +0500

    added work with sqllite db

--- app/src/main/java/com/indigo/first_ui_project/MainActivity.kt
+++ app/src/main/java/com/indigo/first_ui_project/MainActivity.kt
@@ -2,42 +2,43 @@ package com.simple_auth_page
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ListView
-import android.widget.TextView
 import android.widget.Toast
+import com.indigo.first_ui_project.DbHelper
+import com.indigo.first_ui_project.User
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val listView = findViewById<ListView>(R.id.listView)
-        val userData: EditText = findViewById(R.id.user_data)
+        val loginField: EditText = findViewById(R.id.login)
+        val emailField: EditText = findViewById(R.id.email)
+        val passwordField: EditText = findViewById(R.id.password)
         val button: Button = findViewById(R.id.button)
 
-        val todos: MutableList<String> = mutableListOf()
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, todos)
-        listView.adapter = adapter
-
-        listView.setOnItemClickListener {adapterView, view, position, id ->
-            val text = listView.getItemAtPosition(position).toString()
-            adapter.remove(text)
-
-            Toast.makeText(this, "Мы удалили: $text", Toast.LENGTH_LONG).show()
-        }
-
-
         button.setOnClickListener {
-            var text = userData.text.toString().trim()
+            val login = loginField.text.toString().trim()
+            val email = emailField.text.toString().trim();
+            val password = passwordField.text.toString().trim()
 
             if (
-                text != ""
-                && !todos.contains(text)
+                login == ""
+                || email == ""
+                || password == ""
             ) {
-                adapter.insert(text, 0)
+                Toast.makeText(this, "Не все поля заполнены", Toast.LENGTH_LONG).show()
+            } else {
+                val user = User(login, email, password)
+
+                val db = DbHelper(this, null)
+                db.addUser(user)
+                Toast.makeText(this, "Пользователь $login добавлен", Toast.LENGTH_LONG).show()
+
+                loginField.text.clear()
+                emailField.text.clear()
+                passwordField.text.clear()
             }
         }
     }
---
+++ app/src/main/java/com/indigo/first_ui_project/User.kt
@@ -0,0 +1,5 @@
+package com.indigo.first_ui_project
+
+class User(val login: String, val email: String, val password: String) {
+
+}
